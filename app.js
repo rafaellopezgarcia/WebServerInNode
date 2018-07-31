8782574be06f8dab16ebd90e2b58442de89435ee
rafaellopezgarcia <ingenieur.rafael@gmail.com>
2018-07-31 18:53:56 -0500

    commit: to json

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var fs = require('fs');
 
 
 http.createServer(function(req, res){
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
+    // res is a writable stream.
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
 }).listen(1337, '127.0.0.1'); 
