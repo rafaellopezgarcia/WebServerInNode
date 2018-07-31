5604915965b52f88abbaff4ffe1a9e2565eab65e
rafaellopezgarcia <ingenieur.rafael@gmail.com>
2018-07-31 18:23:25 -0500

    Sending HTML with dynamic templates

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-/*
-   From here this is where the rest of the course will focus
-*/
-
-// We get http from the NodeJS core.
+// We get http and fs from the NodeJS core.
 var http = require('http');
+var fs = require('fs');
 
-// This is an event listener
-// This function will be executed when a request happens
+
 http.createServer(function(req, res){
-    // res sends information back.
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world\n My name is Rafael \n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var html = fs.readFileSync('./index.html', 'utf8');
+    var message = 'Hello world...';
+    html = html.replace('{Message}', message);
+    res.end(html);
 
 }).listen(1337, '127.0.0.1');
