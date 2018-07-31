5aec92b513dd71a3feaa9cba2e18086e28495520
rafaellopezgarcia <ingenieur.rafael@gmail.com>
2018-07-31 18:36:39 -0500

    Streams for commnication

--- app.js
+++ app.js
@@ -5,9 +5,5 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync('./index.html', 'utf8');
-    var message = 'Hello world...';
-    html = html.replace('{Message}', message);
-    res.end(html);
-
-}).listen(1337, '127.0.0.1');
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+}).listen(1337, '127.0.0.1'); 
