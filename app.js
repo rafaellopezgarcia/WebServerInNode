92a09b874a9b969a601173927d198650832360e4
rafaellopezgarcia <ingenieur.rafael@gmail.com>
2018-07-31 19:06:35 -0500

    Routing

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
     // res is a writable stream.
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
-    res.end(JSON.stringify(obj));
+
+    if (req.url === '/'){
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+
+    if(req.url === '/api'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
+    res.writeHead(404);
+    res.end();
 }).listen(1337, '127.0.0.1'); 
